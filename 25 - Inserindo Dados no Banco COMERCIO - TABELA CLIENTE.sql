f6091be4dd2fbec60e0771c9a767741246c94220
Vinicius Aznar <148090013+vinicius-maznar@users.noreply.github.com>
2024-01-05 02:52:18 -0300

    Update 25 - Inserindo Dados no Banco COMERCIO - TABELA CLIENTE.sql

--- 25 - Inserindo Dados no Banco COMERCIO - TABELA CLIENTE.sql
+++ 25 - Inserindo Dados no Banco COMERCIO - TABELA CLIENTE.sql
@@ -10,16 +10,16 @@ INSERT INTO CLIENTE VALUES(NULL,'CELIA','M','CELIA@IG.COM.BR','74896512325');
 
 /* OUTPUT
 mysql> INSERT INTO CLIENTE VALUES(NULL,'JOAO','M','JOAO@IG.COM.BR','12378945613');
-Query OK, 1 row affected (0.02 sec)
+Query OK, 1 row affected (0.01 sec)
 
 mysql> INSERT INTO CLIENTE VALUES(NULL,'CARLOS','M','CARLOS@IG.COM.BR','45612378956');
 Query OK, 1 row affected (0.00 sec)
 
 mysql> INSERT INTO CLIENTE VALUES(NULL,'ANA','F','ANA@IG.COM.BR','15975312312');
-Query OK, 1 row affected (0.00 sec)
+Query OK, 1 row affected (0.02 sec)
 
 mysql> INSERT INTO CLIENTE VALUES(NULL,'CLARA','F',NULL,'14785236989');
-Query OK, 1 row affected (0.00 sec)
+Query OK, 1 row affected (0.01 sec)
 
 mysql> INSERT INTO CLIENTE VALUES(NULL,'JORGE','M','JORGE@IG.COM.BR','75689412325');
 Query OK, 1 row affected (0.00 sec)
@@ -29,7 +29,7 @@ Query OK, 1 row affected (0.00 sec)
 */
 
 
-/* PASSO 7: Checando os dados inseridos */
+/* PASSO 2: Checando os dados inseridos */
 
 /* INPUT */
 SELECT * FROM CLIENTE;
@@ -40,11 +40,11 @@ mysql> SELECT * FROM CLIENTE;
 | IDCLIENTE | NOME   | SEXO | EMAIL            | CPF         |
 +-----------+--------+------+------------------+-------------+
 |         1 | JOAO   | M    | JOAO@IG.COM.BR   | 12378945613 |
-|         3 | CARLOS | M    | CARLOS@IG.COM.BR | 45612378956 |
-|         4 | ANA    | F    | ANA@IG.COM.BR    | 15975312312 |
-|         5 | CLARA  | F    | NULL             | 14785236989 |
-|         6 | JORGE  | M    | JORGE@IG.COM.BR  | 75689412325 |
-|         7 | CELIA  | M    | CELIA@IG.COM.BR  | 74896512325 |
+|         2 | CARLOS | M    | CARLOS@IG.COM.BR | 45612378956 |
+|         3 | ANA    | F    | ANA@IG.COM.BR    | 15975312312 |
+|         4 | CLARA  | F    | NULL             | 14785236989 |
+|         5 | JORGE  | M    | JORGE@IG.COM.BR  | 75689412325 |
+|         6 | CELIA  | M    | CELIA@IG.COM.BR  | 74896512325 |
 +-----------+--------+------+------------------+-------------+
-6 rows in set (0.00 sec)
+6 rows in set (0.02 sec)
 */
